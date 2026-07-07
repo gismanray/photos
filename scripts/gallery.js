27a04336eabe6be53c5c86498c8bbbb8f50198f4
gismanray <57723407+gismanray@users.noreply.github.com>
2026-07-07 13:44:38 -0700

    Fix arrow function syntax in event listeners

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -33,13 +33,13 @@ function main() {
 	ctl.addEventListener("click", changeSize);
 	ctl.addEventListener("mouseout", function() { this.style.display='none' });
 	cmt.addEventListener("click", showReview);
-	info.addEventListener("click" => () {
+	info.addEventListener("click" => {
 		msg.style.display = "block";
-	}
+	});
 
-	xmsg.addEventListener("click" => () {
+	xmsg.addEventListener("click" => {
 		msg.style.display = "none";
-	}
+	});
 	
 	var album = getParameter("album");
 	if (album == null) {
