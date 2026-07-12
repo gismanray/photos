dd9dbd12c39d54747de94ae515332f49d80c6095
gismanray <57723407+gismanray@users.noreply.github.com>
2026-07-11 17:16:50 -0700

    Refactor slides3.1.js for improved readability

--- scripts/slides3.1.js
+++ scripts/slides3.1.js
@@ -60,14 +60,14 @@ $(document).ready(function(){
 	});
 
 	$("#info").on("mouseover", function() {
-		$(this).src = "images/infob.png";
+		$(this).attr("src", "images/infob.png");
 	}).on("mouseout", function() {
-		$(this).src = "images/infoa.png";
+		$(this).attr("src", "images/infoa.png");
 	}).on("click", function() {
 		$("#msg").css("display", "block");
 	});
 
-	$("xmsg").on("mouseover", function() {
+	$("#xmsg").on("mouseover", function() {
 		$(this).css("color", "#ff0000");
 	}).on("mouseout", function() {
 		$(this).css("color", "#000000");
@@ -75,7 +75,7 @@ $(document).ready(function(){
 		$("#msg").css("display", "none");
 	});
 
-	$("msg").on("mouseleave", function() {
+	$("#msg").on("mouseleave", function() {
 		$(this).css("display", "none");
 	});
 	
